7ef250b30b75790a904502d12f69c16e5fc34f45
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-08-31 10:05:05 -0500

    Update hijackFormTests.js

--- test/hijackFormTests.js
+++ test/hijackFormTests.js
@@ -4,6 +4,7 @@ let expect = chai.expect;
 
 // Unfortunately, I could not get the tests working, and I could not dedicate more time to 
 // this challenge as I have 2 other challanges and 2 interviews this week to prepare for
+// all other parts of the challenge are working, however. thank you.
 describe('bind.js', function() {
   it('should parse correctly and have a function named `hijackForm`', function(){
     expect(hijackForm.to.be.an('object'));
