b288b1fe45d5fbb410e11b1279b964c8e1665884
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-08-31 11:11:48 -0500

    tests are working

--- test/hijackFormTests.js
+++ test/hijackFormTests.js
@@ -7,4 +7,12 @@ describe('bind.js', function() {
   it('should parse correctly and have a function named `hijackForm`', function(){
     expect(hijackForm).to.be.an('function');
   });
+
+  it('should have a function named `sendData`', function(){
+    expect(sendData).to.be.an('function');
+  });
+
+  it('should have a function named `createPayload`', function(){
+    expect(createPayload).to.be.an('function');
+  });
 });
